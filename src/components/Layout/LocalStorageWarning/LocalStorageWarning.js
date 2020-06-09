35a60efbd149985fcf0b57c8886a019a2fd95d80
aseremak <adam@seremak.org>
2020-06-09 07:47:16 +0200

    Update UserPanel
    -display stars informing about user achivements
    -local storage warning - browser name put into search query (More info)

--- src/components/Layout/LocalStorageWarning/LocalStorageWarning.js
+++ src/components/Layout/LocalStorageWarning/LocalStorageWarning.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './LocalStorageWarning.css';
 
-const localStorageWarning = (props) => (
-	<div className="LocalStorageWarning stdBlockStrong">
-		Web Storage Disabled! Enable this feature to store your best scores!{' '}
-		<a href="https://www.google.com/search?q=how+enable+web+storage+in+chrome" target="_new_tab">
-			More info
-		</a>
-	</div>
-);
+const localStorageWarning = (props) => {
+	let browser = 'Chrome';
+
+	if (navigator.userAgent.indexOf('Opera') !== -1 || navigator.userAgent.indexOf('OPR') !== -1) {
+		browser = 'Opera';
+	} else if (navigator.userAgent.indexOf('Edg') !== -1) {
+		browser = 'Microsoft+Edge';
+	} else if (navigator.userAgent.indexOf('Chrome') !== -1) {
+		browser = 'Chrome';
+	} else if (navigator.userAgent.indexOf('Safari') !== -1) {
+		browser = 'Safari';
+	} else if (navigator.userAgent.indexOf('Firefox') !== -1) {
+		browser = 'Firefox';
+	} else if (navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode === true) {
+		//IF IE > 10
+		browser = 'IE';
+	}
+
+	return (
+		<div className="LocalStorageWarning stdBlockStrong">
+			Web Storage Disabled! Enable this feature to store your best scores!{' '}
+			<a href={'https://www.google.com/search?q=how+enable+web+storage+in+' + browser} target="_new_tab">
+				More info
+			</a>
+		</div>
+	);
+};
 
 export default localStorageWarning;
--- src/components/UserPanel/GameInfo/GameInfo.js
+++ src/components/UserPanel/GameInfo/GameInfo.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import './GameInfo.css';
+import starGrey from '../../../assets/images/star128g.png';
+import starYellow from '../../../assets/images/star128y.png';
+import starInvisible from '../../../assets/images/star128inv.png';
 
 const GameInfo = (props) => {
-	const
-		personalBest = props.personalBest || '-',
-		averagePB = props.averagePB || '-',
+	const personalBest = props.personalBest || '-',
 		highscore = props.highscore || '-';
-	let 
-		classes = 'GameInfo',
-		vertCenterDiv = null;
+	let classes = 'GameInfo',
+		stars = [];
 	if (props.selected) {
-		classes += ' GameInfoSelected'
-	};
+		classes += ' GameInfoSelected';
+	}
 	if (props.header) {
 		classes += ' GameInfoHeader';
-		vertCenterDiv = 'vertCenterDiv';
-	};
-	
+		stars.push(
+			<img key="starPB" src={starGrey} alt="grey star" />,
+			<img key="starHS" src={starGrey} alt="grey star" />
+		);
+	} else if (personalBest !== '-') {
+		stars.push(<img key="starPB" src={starYellow} alt="gold star" />);
+		if (personalBest <= highscore) {
+			stars.push(<img key="starHS" src={starYellow} alt="gold star" />);
+		}
+	} else {
+		stars.push(<img key="starInvisible" src={starInvisible} alt="invisible star" />);
+	}
+
+
 	return (
 		<div className={classes}>
-			<div className="GameInfoName"><div className={vertCenterDiv}>{props.gameLabel}</div></div>
-			<div className="GameInfoUserPB"><div className={vertCenterDiv}>{personalBest}</div></div>
-			<div className="GameInfoAvgPB"><div className={vertCenterDiv}>{averagePB}</div></div>
-			<div className="GameInfoHighscore"><div className={vertCenterDiv}>{highscore}</div></div>
-			<div className="GameInfoStars"><div className={vertCenterDiv}>stars</div></div>
+			<div className="GameInfoName">
+				<div className="vertCenterDiv">{props.gameLabel}</div>
+			</div>
+			<div className="GameInfoUserPB">
+				<div className="vertCenterDiv">{personalBest}</div>
+			</div>
+			<div className="GameInfoHighscore">
+				<div className="vertCenterDiv">{highscore}</div>
+			</div>
+			<div className="GameInfoStars">
+				<div className="vertCenterDiv">{stars}</div>
+			</div>
 		</div>
 	);
 };
